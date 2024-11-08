145e360cff87935ff9fe4633e1d3e8e6983aee39
KaelNielson <kgryffyn@gmail.com>
2024-11-07 20:28:46 -0700

    re-created vite file

--- startupVite/src/App.jsx
+++ startupVite/src/App.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-// import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './login/login';
-import { Play } from './game/game';
-import { Scores } from './score/score';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,18 +8,26 @@ function App() {
 
   return (
     <>
-      <header>
-          <h1>Nielson's Economy</h1>
-          <div class="white_space"></div>
-          <div>
-              <img id="stonkImage" width = "150px" src="https://i.pinimg.com/736x/08/1d/42/081d42bee6b2145dd4afddcbf14553af.jpg" />
-          </div>
-      </header>
-      <main></main>
-      <footer id="infoFooter">
-          <span class="text-reset">Kael Nielson</span>
-          <a href="https://github.com/KaelNielson/startup">Git Repository</a>
-      </footer>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
