a0a238f7c76d06b581f9804ba7200fe651dac771
KaelNielson <kgryffyn@gmail.com>
2024-11-11 14:58:50 -0700

    Started on game functionality, naming doesn't quite work yet

--- startupVite/src/game/game.jsx
+++ startupVite/src/game/game.jsx
@@ -1,52 +1,99 @@
 import React from 'react';
 import "./game.css"
 
+
 export function Game() {
-  return (
-    <main>
-        <div>
-            <h1>Business Name: Best Business</h1>
-        </div>
-        <div className="rows">
-            <div id="infoList" className="items">
-                Stats:
-                <div>Balance: <span className="stat">$2,735</span></div>
-                <div>Income Per Day: <span className="stat">$70</span></div>
-                <div>Costs Per Day: <span className="stat">$35</span></div>
-                <div>Net Per Day: <span className="stat">$35</span></div>
-                <div>Average Wage: <span className="stat">$15</span></div>
-                <div>Employees: <span className="stat">1</span></div>
-                <div>Costumers Per Day: <span className="stat">10</span></div><br />
-                <button id="restartButton">Restart/Delete Business</button><br />
-                <button id="questioningButton">Are you sure?</button>
+  const [exists, setExists] = React.useState(false);
+  const [name, setName] = React.useState("")
+  const [stats, setStats] = React.useState([0,0,0,0,0,0,0])
+
+  function deleteBusiness() {
+    document.getElementById("restartButton").style.display = "none"
+    document.getElementById("questioningButton").style.display = "block"
+  }
+
+  function stepTwo() {
+    return setExists(false)
+  }
+
+  function createName() {
+    let r = Math.random()
+    let newName = ""
+    if (r < 0.5) {
+        newName = "Best Business"
+    } else {
+        newName = "Worst Business"
+    }
+    document.getElementById("nameInput").value = newName;
+    return newName
+  }
+  
+  function createBusiness() {
+    setName(document.getElementById("nameInput").value)
+    if (name === "") {
+        setName(createName());
+    }
+    setStats([0,0,0,0,0,0,0])
+    setExists(true)
+  }
+
+  if (exists == true) {
+    return (
+        <main>
+            <div>
+                <h1>Business Name: {name}</h1>
             </div>
-            <div id="mainEvent" className="items">
-                <p>Balance(dollars) over time(months):</p>
-                <div>3,000</div>
-                <svg id="graph" width="175" height="175" viewBox="0 0 150 150">
-                    <polyline className='border-line' points="0,0 0,150 150,150"/>
-                    <polyline className='graph-line' points="0,150 25,75 50,85 75,75 150,25"/>
-                </svg>
-                <div>0&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;7
+            <div className="rows">
+                <div id="infoList" className="items">
+                    Stats:
+                    <div>Balance: <span className="stat">${stats[0]}</span></div>
+                    <div>Income Per Day: <span className="stat">${stats[1]}</span></div>
+                    <div>Costs Per Day: <span className="stat">${stats[2]}</span></div>
+                    <div>Net Per Day: <span className="stat">${stats[3]}</span></div>
+                    <div>Average Wage: <span className="stat">${stats[4]}</span></div>
+                    <div>Employees: <span className="stat">{stats[5]}</span></div>
+                    <div>Costumers Per Day: <span className="stat">{stats[6]}</span></div><br />
+                    <button id="restartButton" onClick={deleteBusiness}>Restart/Delete Business</button><br />
+                    <button id="questioningButton" onClick={stepTwo}>Are you sure?</button>
+                </div>
+                <div id="mainEvent" className="items">
+                    <p>Balance(dollars) over time(months):</p>
+                    <div>3,000</div>
+                    <svg id="graph" width="175" height="175" viewBox="0 0 150 150">
+                        <polyline className='border-line' points="0,0 0,150 150,150"/>
+                        <polyline className='graph-line' points="0,150 25,75 50,85 75,75 150,25"/>
+                    </svg>
+                    <div>0&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;7
+                    </div>
+                </div>
+                <div id="eventHistory" className="items">
+                    Event History:<br />
+                    <div><span className="time">(3 min ago)</span> Event "<span className="event">Theft!</span>"</div>
+                    <div>You chose the option "<span className="option">More Security</span>" which caused the effect(s):</div>
+                    (<span className="bad">-1 employee</span>, <span className="good">-100 cost per day</span>, <span className="bad">-50 balance</span>)
+                    <div><span className="time">(4 hours ago)</span> Event "<span className="event">Lawsuit over Logo</span>"</div>
+                    <div>You chose the option "<span className="option">Settle</span>" which caused the effect(s):</div>
+                    (<span className="bad">-1,000 balance</span>)
+                    <div><span className="time">(3 days ago)</span> Event "<span className="event">Generous Gift</span>"</div>
+                    <div>you chose the option "<span className="option">Accept</span>" which caused the effect(s):</div>
+                    (<span className="good">+1,643 balance</span>)
+                </div>
+                <div id="eventsQueue" className="items">
+                    Pending Events:<br />
+                    -- NONE --
                 </div>
             </div>
-            <div id="eventHistory" className="items">
-                Event History:<br />
-                <div><span className="time">(3 min ago)</span> Event "<span className="event">Theft!</span>"</div>
-                <div>You chose the option "<span className="option">More Security</span>" which caused the effect(s):</div>
-                (<span className="bad">-1 employee</span>, <span className="good">-100 cost per day</span>, <span className="bad">-50 balance</span>)
-                <div><span className="time">(4 hours ago)</span> Event "<span className="event">Lawsuit over Logo</span>"</div>
-                <div>You chose the option "<span className="option">Settle</span>" which caused the effect(s):</div>
-                (<span className="bad">-1,000 balance</span>)
-                <div><span className="time">(3 days ago)</span> Event "<span className="event">Generous Gift</span>"</div>
-                <div>you chose the option "<span className="option">Accept</span>" which caused the effect(s):</div>
-                (<span className="good">+1,643 balance</span>)
+        </main>
+    );
+  } else {
+    return (
+        <main>
+            <div id="makeDiv">
+            <input id="nameInput" type="text" placeholder="Name your business"></input>
+            <button className="nameButtons" onClick={createName}>Random Name</button>
+            <button className="nameButtons" onClick={createBusiness}>Create Business</button>
             </div>
-            <div id="eventsQueue" className="items">
-                Pending Events:<br />
-                -- NONE --
-            </div>
-        </div>
-    </main>
-  );
+        </main>
+    )
+  }
 }
