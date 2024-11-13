e3af089f7ffa7169f6d7d913ff821ba2c08258b5
KaelNielson <kgryffyn@gmail.com>
2024-11-13 13:13:27 -0700

    multiple events can be pending, buttons now pressable

--- startupVite/src/game/game.jsx
+++ startupVite/src/game/game.jsx
@@ -59,6 +59,10 @@ export function Game() {
         constructor(target, amount) {
             this.target = target;
             this.amount = amount;
+            this.text = this.amount.toString() + this.target;
+            if ((this.target === "income") || (this.target === "costs") || (this.target === "customers")) {
+                this.text += " per day"
+            }
         }
     
         activate() {
@@ -77,25 +81,35 @@ export function Game() {
             } else if (this.target == "paying") {
                 stats.paying = stats.paying + this.amount;
             }
-            stats.totalRecalc()
+            stats.fullRecalc()
         }
     }
     
     class option {
-        constructor(text, effects) {
+        constructor(name, text, effects) {
             this.text = text;
             this.effects = effects;
+            this.name = name
+        }
+
+        toString() {
+            return this.name
         }
     
         choose() {
-            for (let effect in this.effects) {
-                this.effects[effect].activate();
+            let ef = this.effects
+            function f () {
+                for (let effect in ef) {
+                    ef[effect].activate();
+                }
+                setShowing(false)
             }
+            return f
         }
     
         render() {
             return (
-                <div className="optionStyle">{this.text}<br /><button onClick={this.choose}>Accept</button></div>
+                <div className="optionStyle">{this.text}<br /><button onClick={this.choose()}>Accept</button></div>
                 
             )
         }
@@ -141,16 +155,24 @@ export function Game() {
     let e2 = [new effect("balance", 0), new effect("income", 10), new effect("costs", 1000), new effect("employees", 1), new effect("wage", 160), new effect("customers", 50), new effect("paying", 20)]
     let e3 = [new effect("balance", 1000), new effect("income", 0), new effect("costs", 10000), new effect("employees", 10), new effect("wage", 350), new effect("customers", 35), new effect("paying", 850)]
     let e4 = [new effect("balance", 15), new effect("income", 0), new effect("costs", 10), new effect("employees", 0), new effect("wage", 0), new effect("customers", 5), new effect("paying", 500)]
+    let e5 = [new effect("balance", 20)]
+    let e6 = [new effect("customer", 1)]
     let firstText = "You've told your family and friends that you want to start a business, and now everyone wants to be your business mentor."
+    let secondText = "You're walking to your business one day when you see a wallet on the ground. Picking it up, you find 20 dollars and an ID inside. You reach your business and find two customers waiting for you to arrive."
     let op1Text = "Your father approaches you at your desk where you're brainstorming. 'I've heard that Billy down the street is selling his gas station. He's an ol' pal of mine and I bet I could help you get it for a discount. I've done some work in that business in the past too-'"
     let op2Text = "Your mother shoes your father away and says: 'Enough of that nonsense. Me ma just passed away and left her bakery to no one. Surely there be no better boy to run it then you. With me cooking of course."
     let op3Text = "Your rich uncle, who you've only met once, bursts in in a disheveled three-piece suit. It looks like he's run all the way here. 'My little (nephew or niece) is finally an entrepurer!' He shouts. 'Don't worry one bit about starting capital, I'll bankroll you a high-end restaurant. I've got buddies in the chef business, you won't have to worry about a thing.'"
     let op4Text = "Your dropout friend from highschool speaks up from the couch, causing everyone to remember that he's here. 'Listen, man, you don't need to worry about any fancy buildings or merchendise or anything, cause I heard that Ricky left to Wisconsin the other day, so there's no one around to sell...uh...lettuce.' Your parents give him a disapproving side-eye.'"
-    let op1 = new option(op1Text, e1)
-    let op2 = new option(op2Text, e2)
-    let op3 = new option(op3Text, e3)
-    let op4 = new option(op4Text, e4)
+    let op5Text = "One of the customers, a man in a golden tuxedo, notices the wallet. 'Hey, that's good luck. Wish I'd found 20 dollars on the street.'"
+    let op6Text = "The other customer, an elderly woman, looks up from the merchendise and figures out what the man is talking about. 'You're going to pocket the money? But that would be stealing. It's got an ID in it, be sure it's returned properly.'"
+    let op1 = new option("Dad's Gas Station", op1Text, e1)
+    let op2 = new option("Mum's Bakery", op2Text, e2)
+    let op3 = new option("Alfred's Restaurant", op3Text, e3)
+    let op4 = new option("Ricky's Backyard Deals", op4Text, e4)
+    let op5 = new option("Pocket the money.", op5Text, e5)
+    let op6 = new option("Return the wallet.", op6Text, e6)
     let firstEvent = new Event("Starting a business?", firstText, [op1, op2, op3, op4])
+    let secondEvent = new Event("A stroke of luck?", secondText, [op5, op6])
 
     function runAMonth() {
         setMonths(months + 1);
@@ -212,7 +234,7 @@ export function Game() {
         } else {
             setName(v)
         }
-        stats = [0,0,0,0,0,0,0]
+        stats = new statList([0,0,0,0,0,0,0])
         setExists(true)
         let temp = [firstEvent]
         setEvents(pendingEvents.concat(temp))
@@ -237,6 +259,7 @@ export function Game() {
                             <div>Costumers Per Day: <span className="stat">{stats.customer}</span></div><br />
                             <button id="restartButton" onClick={deleteBusiness}>Restart/Delete Business</button><br />
                             <button id="questioningButton" onClick={stepTwo}>Are you sure?</button>
+
                         </div>
                         <div id="mainEvent" className="items">
                             <p>Balance(dollars) over time(months):</p>
